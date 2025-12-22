5f4601916c0b1725c2b538e0a86f63dd0333eaea
oschiessl <oliver.schiessl@pazz.de>
2025-12-22 10:17:03 +0100

    fix(ci): disable TLS on websocket port 5280 to resolve E2E connection failure

--- apps/ngx-chat-e2e-new/src/page-objects/app.po.ts
+++ apps/ngx-chat-e2e-new/src/page-objects/app.po.ts
@@ -12,7 +12,7 @@ const adminLogin: AuthRequest = {
   domain: devXmppDomain,
   username: devXmppJid?.split('@')[0] as string,
   password: devXmppPassword,
-  service: `wss://${devXmppDomain}:5280/websocket`,
+  service: `ws://${devXmppDomain}:5280/websocket`,
 };
 
 export class AppPage {
