666808fedbea4d6b93769aec4196b2072e86e06f
oschiessl <oliver.schiessl@pazz.de>
2025-12-30 21:52:57 +0100

    fix(test): configure websocket service url for roster tests

--- libs/ngx-xmpp/src/test/roster.plugin.spec.ts
+++ libs/ngx-xmpp/src/test/roster.plugin.spec.ts
@@ -22,12 +22,14 @@ const timLogin: AuthRequest = {
   domain: devXmppDomain,
   username: 'tim',
   password: 'tim',
+  service: 'wss://localhost:5280/websocket',
 };
 
 const bobLogin: AuthRequest = {
   domain: devXmppDomain,
   username: 'bob',
   password: 'bob',
+  service: 'wss://localhost:5280/websocket',
 };
 
 describe('roster plugin', () => {
